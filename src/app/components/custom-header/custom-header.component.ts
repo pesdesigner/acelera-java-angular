55a872e1283f27c669c92943e349adc9acf68403
pesdesigner <pauloeduardo.dg@outlook.com>
2024-02-25 10:42:49 -0300

    Criando botão: sair da sessão

--- src/app/components/custom-header/custom-header.component.ts
+++ src/app/components/custom-header/custom-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-custom-header',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class CustomHeaderComponent {
   isCollapsed = true;
+
+  constructor(private router: Router){}
+
+  logout(){
+    localStorage.clear()
+    this.router.navigate(['']);
+  }
 }
